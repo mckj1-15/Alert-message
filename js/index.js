5a13cb2c5a52277c83680ef3c2527eab1172ccaf
mckj1-15 <mckj1_15@uni.worc.ac.uk>
2017-12-04 15:29:19 +0000

    changes

--- js/index.js
+++ js/index.js
@@ -53,7 +53,7 @@ function dialogDismissed(buttonIndex) {
     } 
    	else if(buttonIndex==2) {
         new Toast({content: 'Continue working!.', duration: 3000});
-        createNotification();
+        createNotification2();
         createDialog();
     }
 
@@ -80,5 +80,31 @@ function createNotification() {
         date: 		notificationTime, 
         badge: 		notification_count++
    	});
+    
+    
+    function createNotification2() {
+        		
+	//
+    //generate a time to post notification
+    //
+    var currentTime = new Date().getTime(); //current time
+    var notificationTime = new Date(currentTime + 10000); //delayed time  - add 1 second
+    			
+    //
+    //setup notification
+    //
+    
+    cordova.plugins.notification.local.schedule({ 
+    	id: 		1,
+        title: 		"Hey you",
+        message: 	"This is an example notification",
+        date: 		notificationTime, 
+        badge: 		notification_count++
+   	});
+        
+        cordova.plugins.notification.local.on("click", function (notification) {
+        createDialog();
+            
+        });
     
 }
